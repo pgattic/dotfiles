8232b1ad5d2e079649f743a7149a84dade14f29b
pgattic <pgattic@gmail.com>
2024-01-26 22:25:40 -0700

    Harpoon: add tabline

--- .config/nvim/lua/pgattic/remaps.lua
+++ .config/nvim/lua/pgattic/remaps.lua
@@ -20,3 +20,14 @@ vim.api.nvim_create_autocmd('TermOpen', {
 
 vim.keymap.set("n", "<leader>t", ":sp<CR><C-w><C-r>:resize 12<CR>:terminal<CR>$a")
 
+vim.keymap.set("n", "J", "mzJ`z") -- Line-appending: Keep cursor location
+
+vim.keymap.set({"n", "v"}, "<leader>y", "\"+y") -- System Copy
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("n", "n", "nzzzv") -- Keep searhch terms in the middle of the screen
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "<CR>", ":noh<CR><CR>", {silent = true}) -- Return also removes search highlights
+
+vim.keymap.set("x", "p", "\"_dP")  -- do not modify registers on paste
+
