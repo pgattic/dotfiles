022cfe7596170ffcab035d1529a0129203dc0ea0
pgattic <pgattic@gmail.com>
2024-02-02 10:48:36 -0700

    Persistent workspaces, other sway changes

--- setup-sway.sh
+++ setup-sway.sh
@@ -1,9 +1,15 @@
+# NOTE: The configs depend on JetBrainsMono Nerd Font being installed on the system, and this setup script assumes the dnf package manager, connected to the default Fedora Linux repos.
 
 echo "Setting up Sway..."
 
 echo "Installing Sway-related packages..."
 
-sudo dnf install waybar brightnessctl pulseaudio-utils fuzzel -y
+sudo dnf install sway waybar brightnessctl pulseaudio-utils fuzzel -y
+
+# waybar: Alternative, more customizable statusbar for Wayland
+# brightnessctl: Utility to control screen brightness. Used by sway/config
+# pulseaudio-utils: Utility to control pulseaudio settings. Used by sway/config (provides pactl)
+# fuzzel: Alternative app launcher that works with Flatpak apps and uses fuzzy finding
 
 echo "Done!"
 
