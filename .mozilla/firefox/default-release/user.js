e0dab955b5a28236e4f203ee94dfdfe76943565d
pgattic <pgattic@gmail.com>
2024-02-05 11:06:24 -0700

    Firefox - remove those annoying swipe gestures

--- .mozilla/firefox/default-release/user.js
+++ .mozilla/firefox/default-release/user.js
@@ -1,8 +1,11 @@
 
 user_pref("browser.compactmode.show", true);
+user_pref("browser.gesture.swipe.left", "cmd_scrollLeft");
+user_pref("browser.gesture.swipe.right", "cmd_scrollRight");
 user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
 user_pref("browser.newtabpage.activity-stream.showSponsored", false);
 user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.tabs.inTitlebar", 0);
 user_pref("network.dns.disablePrefetch", true);
 user_pref("network.http.speculative-parallel-limit", 0);
 user_pref("network.predictor.enabled", false);
